9343b1243e1179f81c7dd7e6b7a9c50debd7174e
sukesh <85454574+sukeshperiyasamy@users.noreply.github.com>
2023-03-30 00:11:39 +0530

    2nd

--- day3.c
+++ day3.c
@@ -35,6 +35,31 @@ for(int i=0;i<n;i++)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 2nd
+Air Conditioner Temperature
+
+
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    int a,b,c,d;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d %d %d",&a,&b,&c);
+        if ( a<=b && b>=c)
+        {
+        printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+        
+    }
+		return 0;
+}
 
 
 
