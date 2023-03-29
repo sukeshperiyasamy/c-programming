afa922d1954e591407c18861137e1b7597be5c05
sukesh <85454574+sukeshperiyasamy@users.noreply.github.com>
2023-03-29 23:19:00 +0530

    Update day3.c

--- day3.c
+++ day3.c
@@ -1,26 +1,34 @@
 1 st
 High Accuracy
 
-
-
 #include <stdio.h>
-
 int main(void) {
-int n,a;
-float b,c,d;
+int n,a,b;
+
 scanf("%d",&n);
 
 for(int i=0;i<n;i++)
 {
     scanf("%d",&a);
-     if(a)
-    
-   printf("%.0f\n",);
+     b=a%3;
+     if(b==1)
+     {
+         
+          printf("2\n");
+     }
+     else if(b==2)
+     {
+          printf("1\n");
+     }
+     else
+    {
+        printf("0\n");
+    }
+  
 
 }
-	return 0;
+  return 0;
 }
-
 
 
 
