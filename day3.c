574aa20829a497de477bfb05ff13e716d8db551c
sukesh <85454574+sukeshperiyasamy@users.noreply.github.com>
2023-03-29 22:55:22 +0530

    1st

---
+++ day3.c
@@ -0,0 +1,23 @@
+1 st
+High Accuracy
+
+
+
+#include <stdio.h>
+
+int main(void) {
+int n,a;
+float b,c,d;
+scanf("%d",&n);
+
+for(int i=0;i<n;i++)
+{
+    scanf("%d",&a);
+     if(a)
+    
+   printf("%.0f\n",);
+
+}
+	return 0;
+}
+
